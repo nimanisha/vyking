6e370636926b0e49d3bbe06e8bf681152e773668
C5383033 <nima.akhavi@sap.com>
2025-10-25 01:24:33 +0300

    I ve added all

--- New Approach/terraform/providers.tf
+++ New Approach/terraform/providers.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20"
+      version = "~> 2.0"
     }
     helm = {
-      source = "hashicorp/helm"
-      version = ">= 2.0"
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
     }
   }
 }
