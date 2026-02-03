7c12a9dcaae701fce56f93a98b3c8af7005acd1c
Nima Akhavi <nimaakhavi@Nimas-MacBook-Pro.local>
2026-02-03 19:42:26 +0200

    Updated terraform and also some fixing in deployment backend

--- terraform/phase1/variables.tf
+++ terraform/phase1/variables.tf
@@ -19,16 +19,12 @@
 variable "dockerconfigjson" {
   description = "GitHub token for my account"
   type = string
-  sensitive = true
-  default = "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"
-  
+  sensitive = true  
 }
 variable "postgres_password" {
   description = "DB Password"
   type = string
-  sensitive = true
-  default = "Kia@220995"
-  
+  sensitive = true  
 }
 variable "cluster_name" {
     type = string 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,16 +6,12 @@ variable "deploy_phase2" {
 variable "dockerconfigjson" {
   description = "GitHub token for my account"
   type = string
-  sensitive = true
-  default = "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"
-  
+  sensitive = true  
 }
 variable "postgres_password" {
   description = "DB Password"
   type = string
-  sensitive = true
-  default = "Kia@220995"
-  
+  sensitive = true  
 }
 variable "cluster_name" {
     type = string
