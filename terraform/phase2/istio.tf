42784290efd54db62157a76f10a579fb7d6b7eb3
Nima Akhavi <nimaakhavi@Nimas-MacBook-Pro.local>
2026-03-08 12:59:31 +0200

    Added Istio configuration

---
+++ terraform/phase2/istio.tf
@@ -0,0 +1,15 @@
+resource "helm_release" "istio_base" {
+  name       = "istio-base"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "base"
+  namespace  = "istio-system"
+  depends_on = [kubernetes_namespace.ns]
+}
+
+resource "helm_release" "istiod" {
+  name       = "istiod"
+  repository = "https://istio-release.storage.googleapis.com/charts"
+  chart      = "istiod"
+  namespace  = "istio-system"
+  depends_on = [helm_release.istio_base]
+}
